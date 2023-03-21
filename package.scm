1005d08c3e5d0dac348ceffbaee4def6a1e15a41
Skyler Ferris <skyvine@protonmail.com>
2023-03-21 15:43:10 -0700

    Add make.scm package definition, fix uses of local-file
    I was using the , operator on local-file calls in build definitions.
    This sometimes works, but sometimes results in a local-file *struct*
    appearing in the builder code (instead of the path to the existing
    file). I'm not sure why the behavior is inconsistent, but examining uses
    of local-file in gnu/packages shows that the idiom is to add the
    local-file struct to the native-inputs field, gexp the modify-phases
    form, and ungexp the instance of local-package.

--- package.scm
+++ package.scm
@@ -21,80 +21,102 @@
 
 (define version "0.0")
 
-(define base-guile-code (guix:package
-	(name        "base-guile-code")
-	(version     version)
-	(source      #f)
-	(description #f)
-	(synopsis    #f)
-	(home-page   #f)
-	(license     #f)
+(define base-guile-code
+	(let ((guile-src (guix:local-file "src/scheme/guile/base" #:recursive? #t))
+	      (r7rs-src  (guix:local-file "src/scheme/r7rs/base" #:recursive? #t)))
+		(guix:package
+			(name        "base-guile-code")
+			(version     version)
+			(source      #f)
+			(description #f)
+			(synopsis    #f)
+			(home-page   #f)
+			(license     #f)
 
-	(build-system guix:guile-build-system)
+			(build-system guix:guile-build-system)
 
-	(native-inputs (list
-		guix:guile-3.0-latest
-	))
+			(native-inputs (list guix:guile-3.0-latest guile-src r7rs-src))
 
-	(arguments `(
-		#:not-compiled-file-regexp "(guix/.*.scm|make.scm)"
+			(arguments (list
+				#:not-compiled-file-regexp "(guix/.*.scm|make.scm)"
 
-		#:modules ((guix build utils) ,@guix:%guile-build-system-modules)
+				#:modules `((guix build utils) ,@guix:%guile-build-system-modules)
 
-		#:phases (modify-phases (@ (guix build guile-build-system) %standard-phases)
-			(delete 'unpack)
-			(add-before 'set-locale-path 'fix-paths
-				; Paths in the filesystem are sensible for editing, but not a useful
-				; module structure inside an actual implementation
-				(lambda* (#:key inputs #:allow-other-keys)
-					(use-modules (guix build utils))
-					(let ((guile-src ,(guix:local-file "src/scheme/guile/base" #:recursive? #t))
-					      (r7rs-src ,(guix:local-file "src/scheme/r7rs/base" #:recursive? #t))
-					     )
-						(copy-recursively guile-src "./skyler")
-						(copy-recursively r7rs-src "./skyler/r7rs")
-			)))
-		)
-	))
-))
+				#:phases #~(modify-phases (@ (guix build guile-build-system) %standard-phases)
+					(delete 'unpack)
+					(add-before 'set-locale-path 'fix-paths
+						; Paths in the filesystem are sensible for editing, but not a useful
+						; module structure inside an actual implementation
+						(lambda* (#:key inputs #:allow-other-keys)
+							(use-modules (guix build utils))
+							(copy-recursively #$guile-src "./skyler")
+							(copy-recursively #$r7rs-src "./skyler/r7rs")))))))))
 
-(define guix-code (guix:package
-	(name        "guix-code")
-	(version     version)
-	(source      #f)
-	(description #f)
-	(synopsis    #f)
-	(home-page   #f)
-	(license     #f)
+(define guix-code
+	(let ((guix-code (guix:local-file "src/scheme/guile/guix" #:recursive? #t)))
+		(guix:package
+			(name        "guix-code")
+			(version     version)
+			(source      #f)
+			(description #f)
+			(synopsis    #f)
+			(home-page   #f)
+			(license     #f)
 
-	(build-system guix:guile-build-system)
+			(build-system guix:guile-build-system)
 
-	(native-inputs (list guix:guile-3.0-latest))
+			(native-inputs (list guix:guile-3.0-latest))
 
-	; need to propagate because we're not compiling, see also the note on the
-	; #:not-compiled-file-regexp argument
-	(propagated-inputs (list
-		guix:guile-gcrypt
-		base-guile-code
-	))
+			; need to propagate because we're not compiling, see also the note on the
+			; #:not-compiled-file-regexp argument
+			(propagated-inputs (list
+				guix:guile-gcrypt
+				base-guile-code
+			))
 
-	(arguments `(
-		; don't compile anything, we always want to use the system's guix, not some snapshot
-		#:not-compiled-file-regexp ".*"
+			(arguments (list
+				; don't compile anything, we always want to use the system's guix, not some snapshot
+				#:not-compiled-file-regexp ".*"
 
-		#:phases (modify-phases (@ (guix build guile-build-system) %standard-phases)
-			(delete 'unpack)
-			(add-before 'set-locale-path 'fix-paths
-				; Paths in the filesystem are sensible for editing, but not a useful
-				; module structure inside an actual implementation
-				(lambda* (#:key inputs #:allow-other-keys)
-					(use-modules (guix build utils))
-					(let ((guile-src ,(guix:local-file "src/scheme/guile/guix" #:recursive? #t)))
-						(copy-recursively guile-src "./skyler/guix")
-			)))
-		)
-	))
-))
+				#:phases #~(modify-phases (@ (guix build guile-build-system) %standard-phases)
+					(delete 'unpack)
+					(add-before 'set-locale-path 'fix-paths
+						; Paths in the filesystem are sensible for editing, but not a useful
+						; module structure inside an actual implementation
+						(lambda* (#:key inputs #:allow-other-keys)
+							(use-modules (guix build utils))
+							(copy-recursively #$guix-code "./skyler/guix")))))))))
+
+(define make.scm
+	(let ((src (local-file "src/bin/make.scm")))
+		(guix:package
+			(name        "make.scm")
+			(version     "0.1")
+			(home-page   #f)
+			(synopsis    #f)
+			(description #f)
+			(license     #f)
+
+			(build-system  guix:gnu-build-system)
+			(source        #f)
+			(native-inputs (list guix:guile-3.0-latest (local-file "src/bin/make.scm")))
+
+			(arguments (list
+				#:phases
+				#~(modify-phases (@ (guix build gnu-build-system) %standard-phases)
+					(delete  'unpack)
+					(delete  'configure)
+					(delete  'build)
+					(delete  'check)
+					(replace 'install
+						(lambda* (#:key outputs #:allow-other-keys)
+							(let ((bin-dir (string-append (assoc-ref outputs "out")
+							                              "/bin"))
+							      (make.scm #$(local-file "src/bin/make.scm")))
+								(use-modules (guix build utils))
+								(mkdir-p bin-dir)
+								(copy-recursively make.scm
+								                  (string-append bin-dir "/make.scm")))))))))))
 
 (define personal-code (guix:package
 	(name              "personal-code")
@@ -106,7 +128,7 @@
 
 	(build-system      guix:trivial-build-system)
 	(source            #f)
-	(propagated-inputs (list base-guile-code guix-code))
+	(propagated-inputs (list base-guile-code guix-code make.scm))
 	(arguments         `(#:builder (begin (mkdir (assoc-ref %outputs "out")))))))
 
 personal-code
