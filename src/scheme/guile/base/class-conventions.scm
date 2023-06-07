f122c67adf748f6fc06a2716a242b59ffc586bf7
Skyler Ferris <skyvine@protonmail.com>
2023-06-07 16:06:05 -0700

    Add license info to files missing it.

--- src/scheme/guile/base/class-conventions.scm
+++ src/scheme/guile/base/class-conventions.scm
@@ -1,3 +1,17 @@
+; Copyright 2023 Skyler Ferris
+;
+; This program is free software: you can redsitribute it and/or modify it under the terms
+; of the GNU Affero General Public License as published by the Free Software Foundation,
+; either version 3 of the License or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful, but WITHOUT ANY
+; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+; PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+;
+; You should have received a copy of the GNU Affero General Public License along with this
+; program. If not, see <https://www.gnu.org/licenses>.
+
+; # Documentation
 ; There are a few conventions that I follow when creating GOOPS classes. For the most
 ; part, these conventions are just programming patterns that do not require rigid
 ; formalization through code. However, there are some helper functions that are useful
--- src/scheme/guile/base/serialization/srfi-19.scm
+++ src/scheme/guile/base/serialization/srfi-19.scm
@@ -1,3 +1,16 @@
+; Copyright 2023 Skyler Ferris
+;
+; This program is free software: you can redsitribute it and/or modify it under the terms
+; of the GNU Affero General Public License as published by the Free Software Foundation,
+; either version 3 of the License or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful, but WITHOUT ANY
+; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+; PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+;
+; You should have received a copy of the GNU Affero General Public License along with this
+; program. If not, see <https://www.gnu.org/licenses>.
+
 ; Add serialization support for srfi-19 dates. They use the symbol <srfi-19-date> as a
 ; marker, and a ISO 8610 date/time+zone formatted string to store the value.
 
