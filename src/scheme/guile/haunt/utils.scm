ddfc8cc154228f0463d5f661ecd7941b56d7d64d
Skyler Ferris <skyvine@protonmail.com>
2023-11-12 09:01:21 -0800

    Use a per-invocation directory for signature output
    The signed posts were previously all placed in the same directory,
    under the reasoning that it was static data. However, from the
    perspective of a user-agent, it is far more convenient for the source to
    be placed in the same directory as the HTML. Additionally, this avoids
    any potential problems if there are sources with the same filename in
    different directories.

--- src/scheme/guile/haunt/utils.scm
+++ src/scheme/guile/haunt/utils.scm
@@ -19,16 +19,13 @@
 (define-module (skyler haunt utils)
 	#:use-module ((haunt artifact)   #:prefix haunt.)
 	#:use-module ((haunt post)       #:prefix haunt.)
+	#:use-module ((haunt site)       #:prefix haunt.)
 
 	#:use-module (skyler standard)
 
 	#:export (
-		signature-directory
-		; A parameter containing a string specifying the directory the signed source will be
-		; placed in.
-
 		signed-source
-		; Signature: (signed-source source-file-name)
+		; Signature: (signed-source source-file-name output-directory)
 		;
 		; Arguments:
 		; source-file-name: The file name of the source which should be copied into the
@@ -37,36 +34,49 @@
 		;                   signed-posts builder. There must be a file with the same name and
 		;                   a .sig extension added (not replaced).
 		;
+		; output-directory: The directory of the website where the files should be placed.
+		;                   This is similar to the #:prefix argument of Haunt's blog
+		;                   procedure.
+		;
 		; Return:
 		; A list of 2 Haunt artifacts, representing a copy of the source and the sig file
-		; inside the signature-directory.
+		; inside the output-directory.
 
 		signed-posts
 		; A Haunt builder which will place a copy of the source for each post file as well as
-		; its signature into the directory specified by the signature-directory parameter.
+		; its signature into a directory which mirrors the path of the the source file,
+		; relative to the #:posts-directory argument of the site.
 
 		signature-file-filter
 		; A function that can be used in the #:file-filter argument to a Haunt site so that
 		; Haunt does not try to process the signature files as posts.
 ))
 
-(define signature-directory (make-parameter "/signed-source"))
-
-(define* (signed-source source-file-name)
+(define* (signed-source source-file-name output-directory)
 	(unless (eq? 0 (system* "%%gnupg /bin/gpg%%"
 	                        "--verify" (string-append source-file-name ".sig")))
 		(format #t "ERROR: ~A signature does not match" source-file-name)
 		(exit -1))
 
-	(list (haunt.verbatim-artifact source-file-name
-	                               (string-append (signature-directory)
-	                                              "/" source-file-name))
-	      (haunt.verbatim-artifact (string-append source-file-name ".sig")
-	                               (string-append (signature-directory)
-	                                              "/" source-file-name ".sig"))))
+	(let ((source-basename (basename source-file-name)))
+		(list (haunt.verbatim-artifact source-file-name
+		                               (string-append output-directory "/" source-basename))
+		      (haunt.verbatim-artifact (string-append source-file-name ".sig")
+		                               (string-append output-directory
+		                                              "/" source-basename ".sig")))))
 
 (define* (signed-posts site posts)
-	(flatten (map signed-source (map haunt.post-file-name posts))))
+	(define (post->output-directory post)
+		(let ((source-directory (dirname (haunt.post-file-name post)))
+		      (post-directory   (haunt.site-posts-directory site)))
+			(if (string-prefix? post-directory source-directory)
+				(string-drop source-directory (string-length post-directory))
+				source-directory)))
+
+	(flatten (map (lambda (post)
+	              	(signed-source (haunt.post-file-name post)
+	              	               (post->output-directory post)))
+	              posts)))
 
 (define (signature-file-filter file-name)
 	(not (string-suffix? ".sig" file-name)))
