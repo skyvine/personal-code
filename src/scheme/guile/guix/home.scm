8f6648e407bd344354567571d7a60d47c6a94ce7
Skyler Ferris <skyvine@protonmail.com>
2024-02-23 13:30:47 -0800

    Update aliases for managing guix checkout.

--- src/scheme/guile/guix/home.scm
+++ src/scheme/guile/guix/home.scm
@@ -46,8 +46,10 @@
 				(guix.home-bash-configuration
 					(aliases '(
 						("ls" . "ls --color=auto")
-						("authenticate-guix-checkout" .
-						 "guix git authenticate 9edb3f66fd807b096b48283debdcddccfea34bad 'BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA'")
+						("pull-guix" .
+						 "git pull && guix git authenticate 9edb3f66fd807b096b48283debdcddccfea34bad 'BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA'")
+						("build-guix" .
+						 "guix shell -D guix -CPW -- make")
 					))
 					(guix-defaults? #t)))
 			(guix.simple-service 'custom-env-vars
